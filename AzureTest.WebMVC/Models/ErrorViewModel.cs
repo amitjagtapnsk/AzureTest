dbb5b6267a863eb10a6f9c23a7b983e3a70af0e4
manauto <jagtaprupaliamit@gmail.com>
2023-08-26 21:02:09 +0530

    Add project files.

---
+++ AzureTest.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureTest.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
